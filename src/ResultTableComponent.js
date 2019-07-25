7e7c2e37a5de4ae13ef1f868ef2a3341d5b2490f
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2019-07-25 01:48:48 -0400

    Make minor changes to table view

--- src/ResultTableComponent.js
+++ src/ResultTableComponent.js
@@ -21,7 +21,7 @@ class ResultTableComponent extends React.Component {
           <table id="dtVerticalScrollExample" className="table table-striped">
             <thead className="primary-color white-text">
             <tr>
-              <th scope="col"/>
+              <th scope="col">#</th>
               <th scope="col">Book Name</th>
               <th scope="col">Author</th>
               <th scope="col">Category</th>
@@ -40,7 +40,8 @@ class ResultTableComponent extends React.Component {
                     <span className="badge badge-danger">Remove</span> :
                     <span className="badge badge-success">Add to cart</span>}</a>
                   </h6>
-                  <h6><a href={book.Links[0]} target="_blank"><span className="badge badge-info">Download</span></a></h6>
+                  <h6><a href={book.Links[0]} target="_blank"><span className="badge badge-info">Download</span></a>
+                  </h6>
                 </td>
               </tr>
             )}
